e4ff7d1174778ae78aa842621d707c9f1ea98896
Logan Guthrie <logan@logan.codes>
2017-07-26 10:50:54 -0700

    Initializes Downloads class

---
+++ Downloads.php
@@ -0,0 +1,95 @@
+<?php
+
+class Downloads {
+
+    protected $width;
+
+    /**
+     * Downloads constructor
+     *
+     * @param $columns
+     * @internal param int $width
+     */
+    public function __construct($columns) {
+        $this->width = $this->widthConverter($columns);
+    }
+
+    /**
+     * Converts column numbers to flexboxgrid/bootstrap
+     *
+     * Defaults to 3 columns
+     *
+     * @param $columns int
+     *
+     * @return int
+     */
+    private function widthConverter($columns) {
+        switch ($columns) {
+            case 1:
+                return 12;
+                break;
+            case 2:
+                return 6;
+                break;
+            case 3:
+                return 4;
+                break;
+            case 6:
+                return 2;
+                break;
+            case 12:
+                return 1;
+                break;
+            default:
+                return 4;
+        }
+    }
+
+
+    /**
+     * For showing/hiding block
+     *
+     * @return bool
+     */
+    public function isThereDownloads() {
+        $query = new WP_Query('post_type', 'downloads');
+
+        if ($query->have_posts() === TRUE) {
+            return TRUE;
+        }
+
+        return FALSE;
+    }
+
+    /**
+     * @return string
+     */
+    public function callDownloadsLoop() {
+        $args = array(
+            'post_type' => 'downloads',
+            'posts_per_page' => -1,
+            'orderby' => 'menu_order',
+            'order' => 'ASC'
+        );
+
+        $query = new WP_Query($args);
+        while ($query->have_posts()) : $query->the_post(); ?>
+
+            <div class="col-sm-<?php echo $this->width; ?>">
+                <div class="downloads-img">
+
+                    <?php if (has_post_thumbnail()) : the_post_thumbnail(); endif; ?>
+
+                </div>
+
+                <div class="downloads-text">
+
+                    <?php if (has_excerpt()) : the_excerpt(); endif; ?>
+
+                </div>
+            </div>
+
+        <?php endwhile;
+    }
+
+}
